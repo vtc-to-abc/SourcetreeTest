ad767664fd4be20c1e0e7795e149066fec8eaccf
vtc-to-abc <vuthanhcong31998@gmail.com>
2022-04-18 13:26:25 +0700

    we

--- BlazorServer/Data/ISqlDataAccess.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorServer.Data
-{
-    public interface ISqlDataAccess
-    {
-        Task<List<T>> LoadData<T, U>(string sql, U parameters);
-
-        Task SaveData<T>(string sql, T parameters);
-    }
-}
